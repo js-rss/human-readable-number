49407d861a602d37d620395f66e3c97ccdcce505
js-rss <137919642+js-rss@users.noreply.github.com>
2023-08-04 01:58:20 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function toReadable (number) {
-  
+   const lowNums = ['',' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine',' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen',' seventeen', ' eighteen', ' nineteen'];
+    const hightNums = ['','',' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty',' ninety']; 
+    const hundred = ' hundred';
+
+     if(number === 0){
+        return 'zero'
+    } else if ( number < 20 ){
+        return lowNums[number].substring(1) 
+    } else if ( number < 100) {
+        return hightNums[parseInt(number.toString()[0])].substring(1)  + lowNums[parseInt(number.toString()[1])]
+    } else if(number > 100 && parseInt(number.toString().substring(1)) < 20) {
+        return  lowNums[parseInt(number.toString()[0])].substring(1) + hundred+ lowNums[parseInt(number.toString().substring(1))]
+    } else {
+        return  lowNums[parseInt(number.toString()[0])].substring(1) + hundred + hightNums[parseInt(number.toString()[1])] + lowNums[parseInt(number.toString()[2])]
+    }
 }
